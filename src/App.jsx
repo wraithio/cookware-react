636a66daa1cb4f375e7145b0c1ac31cd01c961f8
wraithio <aarmon5@gmail.com>
2025-02-17 23:50:41 -0800

    continuaing down page and card formatting

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,63 @@ function App() {
     <>
       <div className="mx-auto">
         <h1 className="text-6xl mt-28 mb-8">Start Cooking</h1>
-        <div className="flex justify-evenly gap-6">
-          <CardComponent src={"/images/StartCooking1.jpeg"} title={"Cast Iron Pot"} width={"80"} height={"80"}/>
-          <CardComponent src={"/images/StartCooking2.jpeg"} title={"Stock Pot"} width={"80"} height={"80"}/>
-          <CardComponent src={"/images/StartCooking3.jpeg"} title={"Sauce Pan"} width={"80"} height={"80"}/>
-          <CardComponent src={"/images/StartCooking4.jpeg"} title={"Accessories"} width={"80"} height={"80"}/>
-
+        <div className="flex row">
+          <div className="col-3 flex justify-center">
+            <CardComponent
+              src={"/images/StartCooking1.jpeg"}
+              title={"Cast Iron Pot"}
+              width={"96"}
+              height={"96"}
+            />
+          </div>
+          <div className="col-3 flex justify-center">
+            <CardComponent
+              src={"/images/StartCooking2.jpeg"}
+              title={"Stock Pot"}
+              width={"96"}
+              height={"96"}
+            />
+          </div>
+          <div className="col-3 flex justify-center">
+            <CardComponent
+              src={"/images/StartCooking3.jpeg"}
+              title={"Sauce Pan"}
+              width={"96"}
+              height={"96"}
+            />
+          </div>
+          <div className="col-3 flex justify-center">
+            <CardComponent
+              src={"/images/StartCooking4.jpeg"}
+              title={"Accessories"}
+              width={"96"}
+              height={"96"}
+            />
+          </div>
+        </div>
+        <h1 className="text-6xl mt-28 mb-8">New Arrivals</h1>
+        <div className="row flex">
+          <div className="col-6">
+            <CardComponent
+              src={"/images/NewArrivals1.jpg"}
+              title={"Fall Collection"}
+              width={"full"}
+              height={"96"}
+            />
+            <h3 className="text-3xl text-start">Our 2023 Fall Collection just came out with beautiful autumn color cookware.</h3>
+            <h3 className="text-3xl text-start">Color: <b>Apple Cinnamon</b></h3>
+            <div className="flex">
+            <div className="circle-red">
+              <div className="circle-border"></div>
+            </div>
+            <div className="circle-orange"></div>
+            <div className="circle-yellow"></div>
+            <div className="circle-grey"></div>
+          </div>
+          </div>
+          <div className="col-6">
+            <CardComponent src={"/images/NewArrivals2.jpg"} title={"Nonstick Wok 14 Inch"} width={"full"} height={"96"}/>
+          </div>
         </div>
       </div>
     </>
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const CardComponent = ({src,title,width,height}) => {
-    let cardSize = `w-${width} h-${height} rounded-4` 
+    let cardSize = `h-${height} w-${width}` 
   return (
-    <div>
+    <div className={cardSize}>
         <img src={src} alt={title} className={cardSize}/>
         <h2 className='text-5xl text-start my-6'>{title}</h2>
     </div>
